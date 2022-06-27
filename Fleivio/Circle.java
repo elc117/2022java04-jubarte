4dcc9a81ebed2494e9f71529bbd87264d9b5d0ce
Fleivio <65024539+Fleivio@users.noreply.github.com>
2022-06-27 16:13:51 -0300

    Add files via upload

---
+++ Fleivio/Circle.java
@@ -0,0 +1,15 @@
+package java04;
+
+class Circle extends Shape{
+    private Double radius;
+
+    public Circle(Double r){
+        super("Circulo", 2);
+        radius = r;
+    }
+
+    public Double getArea(){
+        return 2 * Math.PI * Math.pow(radius,2);
+    }
+
+}
---
+++ Fleivio/Shape.java
@@ -0,0 +1,28 @@
+package java04;
+
+class Shape{
+    protected String name;
+    protected int dimension;
+
+    public Shape(String n, int d){
+        name = n;
+        dimension = d;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getDimension(){
+        return dimension;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setDimension(int d){
+        dimension = d;
+    }
+}
+
